f06ec0475c9d203800c1b5c995f9f230461a0c68
Thiago Dorville <dorville.dev@gmail.com>
2023-09-05 11:03:21 -0700

    feat: adicionado componente de backarrow

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import "../index.css";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./modules/Root/Page.tsx";
+import Credits from "./modules/Credits/Page.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root/>,
   },
+  {
+    path: "/credits",
+    element: <Credits/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(

8314f72c503e1c681ff003e401168b4836162fa8
DorvilleThiago <thiago.dorville@souunit.com.br>
2023-09-14 20:42:23 -0300

    fix: scrolls aparecendo no laptop

--- src/modules/Credits/sessions/CollaboratorsContainer.tsx
+++ src/modules/Credits/sessions/CollaboratorsContainer.tsx
@@ -18,8 +18,8 @@ export default function CollaboratorsContainer() {
 
     return (
         <motion.div
-            initial={{ y: 100, opacity: 0}}
-            animate={{ y: 0, opacity: 100}}
+            initial={{ y: 50, opacity: 0}}
+            animate={{ y: 0, opacity: 50}}
             className={`bg-primary max-w-[1053px] p-[40px] h-[600px] w-full max-[1150px]:mx-10 rounded-xl cartoon-shadow ${windowDimensions.width <= 755 ? 'overflow-y-scroll' : ''}`}>
             <div className="h-min w-full flex flex-wrap justify-around gap-14">
                 {Collaborators.map((collaborator, index) => 

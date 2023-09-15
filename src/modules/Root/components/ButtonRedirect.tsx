8f3e6126d478c4ecce5c1765dfac8d151357ae27
DorvilleThiago <thiago.dorville@souunit.com.br>
2023-09-14 21:12:02 -0300

    feat: estilização do scroll

--- src/modules/Root/components/ButtonRedirect.tsx
+++ src/modules/Root/components/ButtonRedirect.tsx
@@ -15,7 +15,7 @@ export default function ButtonRedirect({page, title, mobile = false}: ButtonRedi
                 onClick={() => {
                     navigate(page);
                 }}
-                className={`relative w-[200px] h-[61px] no-flicker`}>
+                className={`relative w-[200px] h-[61px] no-flicker hover:scale-105 active:scale-75 transition-transform`}>
                 <div className={`absolute w-[200px] h-[61px] bg-[#453C85] rounded-[17px] -z-10 right-3 bottom-3`}/>
                 <button
                     className={`bg-[#6456C8] text-background w-[200px] no-flicker h-[61px] rounded-[17px] text-[20px] font-extrabold tracking-wide`}

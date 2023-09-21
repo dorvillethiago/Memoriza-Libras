6d0ba4cc0f96edefe08d20820a009cc8ffde4c6c
thiagodorville <thiago@fontedeprecos.com.br>
2023-09-21 16:10:24 -0300

    refactor: backarrow nav

--- src/modules/Credits/sessions/BackArrowContainer.tsx
+++ src/modules/Credits/sessions/BackArrowContainer.tsx
@@ -2,11 +2,11 @@ import BackArrow from "../components/BackArrow";
 import { motion } from "framer-motion";
 export default function BackArrowContainer() {
     return (
-        <motion.div
+        <motion.nav
             initial={{ y: 100, opacity: 0}}
             animate={{ y: 0, opacity: 100}}
             className="p-6">
             <BackArrow/>
-        </motion.div>
+        </motion.nav>
     )
 }

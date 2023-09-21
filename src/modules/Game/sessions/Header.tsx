1cd1f55f346d0748bb0877997f90b9092aef9a2a
DorvilleThiago <thiago.dorville@souunit.com.br>
2023-09-21 19:17:09 -0300

    feat: adicionado botão de cola

--- src/modules/Game/sessions/Header.tsx
+++ src/modules/Game/sessions/Header.tsx
@@ -1,5 +1,6 @@
 import BackArrow from "../components/BackArrow.tsx";
 import {motion} from "framer-motion";
+import LibraSymbomButton from "../components/LibraSymbolButton.tsx";
 
 export default function Header() {
     
@@ -7,8 +8,9 @@ export default function Header() {
         <motion.header
             initial={{ y: 100, opacity: 0}}
             animate={{ y: 0, opacity: 100}}
-            className={`w-full p-6 flex-col`}>
+            className={`w-full p-6 flex flex-row justify-between`}>
             <BackArrow/>
+            <LibraSymbomButton onClick={() => {}}/>
         </motion.header>
     )
 }
